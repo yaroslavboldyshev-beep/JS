1d097c58660b3950ac1e6b34cf0e202d31be5da0
Yaroslav <yaroslav.boldyshev@vdabcampus.be>
2026-01-08 16:52:09 +0100

    add script

--- blad-steen-schaar/index.js
+++ blad-steen-schaar/index.js
@@ -1,2 +1,60 @@
 "use strict";
-console.log(123);
+const knoppen = document.querySelectorAll(".keuze");
+const winnarEl = document.getElementById("winnar");
+const opnieuwBtn = document.getElementById("opnieuw");
+const resultaatWikkelEl = document.getElementById("resultaatWikkel");
+const computerKeuzeRefEl = document.getElementById("computerKeuze");
+
+let gebruikerKeuze;
+let computerKeuze;
+
+const knoppenVerbergen = () =>
+  knoppen.forEach((el) => {
+    if (el.id !== gebruikerKeuze) el.classList.add("verborgen");
+  });
+
+const winnarWinden = () => {
+  if (gebruikerKeuze == computerKeuze) return "Gelijkspel";
+
+  if (
+    (gebruikerKeuze === "steen" && computerKeuze === "schaar") ||
+    (gebruikerKeuze === "schaar" && computerKeuze === "blad") ||
+    (gebruikerKeuze === "blad" && computerKeuze === "steen")
+  ) {
+    return "Gebruiker";
+  }
+
+  return "Computer";
+};
+
+const computerKeuzeMaken = () => {
+  const index = Math.floor(Math.random() * 3);
+  computerKeuze = knoppen[index].id;
+
+  const afbeelding = document.createElement("img");
+  afbeelding.src = `afbeeldingen/${computerKeuze}.svg`;
+  afbeelding.alt = computerKeuze;
+
+  resultaatWikkelEl.classList.remove("verborgen");
+  computerKeuzeRefEl.insertAdjacentElement("afterend", afbeelding);
+
+  const winnar = winnarWinden();
+
+  winnarEl.textContent = winnar;
+};
+
+const verwerkKlik = (e) => {
+  gebruikerKeuze = e.currentTarget.id;
+  knoppenVerbergen();
+  computerKeuzeMaken();
+};
+
+const opnieuwStarten = () => {
+  location.reload();
+};
+
+knoppen.forEach((el) => {
+  el.addEventListener("click", verwerkKlik);
+});
+
+opnieuwBtn.addEventListener("click", opnieuwStarten);
